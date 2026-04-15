c4d5d77cd8b4e4c17a0bf28f4e48dc367e74f231
ElCociner00 <santiagoelchameluco@gmail.com>
2026-04-15 13:29:10 -0500

    Delete js directory

--- js/billing_config.js
+++
@@ -1,2 +0,0 @@
-export const BILLING_FACTURACION_URL = "/Plataforma_Restaurantes/facturacion/";
-export const BILLING_PAYMENT_URL = "https://mpago.li/15d6BkC";
--- js/configuracion.js
+++
@@ -1,3 +0,0 @@
-// Módulo reservado para futuras mejoras de configuración.
-// Se mantiene intencionalmente ligero para conservar limpia la vista principal
-// de acordeones y redirecciones.
